3227f19d942aae76ec7f238044c7bb624862138d
dani campdelacreu <122914770+danicampdelacreu@users.noreply.github.com>
2023-09-08 17:00:04 +0200

    Array method .some . inlcudes

--- 12-ArrayMethods/js/01-app.js
+++ 12-ArrayMethods/js/01-app.js
@@ -8,3 +8,36 @@ const carrito = [
     { nombre: 'Teclado', precio: 400 },
     { nombre: 'Celular', precio: 700 },
 ]
+
+ // ver todo lo del array
+ meses.forEach((mes) => {
+    console.log(mes);
+})
+
+// comprobar si un valor exciste en el array diferentes maneras de saberlo
+meses.forEach((mes) =>{
+    if( mes === 'Enero'){
+        console.log('Enero si exciste');
+    }
+})
+
+const resultado = meses.includes('Enero');
+console.log(resultado);
+// .includes solo funciona en un arrey de un indice
+
+// en array de objetos se utiliza .some
+const exsiste = carrito.some((producto) =>{
+    return producto.nombre === 'Televisor Curved'
+   
+})
+    console.log(exsiste);
+
+const exsiste2 = meses.some ( mes => mes === 'Febrero');
+console.log(exsiste2);
+
+
+   
+
+
+
+
