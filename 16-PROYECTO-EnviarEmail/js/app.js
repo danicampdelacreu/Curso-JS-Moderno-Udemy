85d6fe5213d5a035d2f48a2de5184ffc9fcd8427
dani campdelacreu <122914770+danicampdelacreu@users.noreply.github.com>
2023-09-26 20:50:02 +0200

    Reto, implementar nuevo input para emailCC

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const email = {
         email: '',
+        emailCC: '',
         asunto: '',
         mensaje: ''
     }
@@ -11,6 +12,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Seleccionar elementos interfaz
     const inputEmail = document.querySelector('#email');
+    const inputEmailCC = document.querySelector('#emailCC') // parte reto implementar CC
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
     const formulario = document.querySelector('#formulario');
@@ -28,10 +30,11 @@ document.addEventListener('DOMContentLoaded', function () {
     inputEmail.addEventListener('input', validar);// llamamos a la funcion cuando suceda el evento si ponemos validar() la llamamos sin que suceda el evento
     inputAsunto.addEventListener('input', validar);
     inputMensaje.addEventListener('input', validar);
+    inputEmailCC.addEventListener('input', validar);// parte de reto implementamos email CC 
 
     formulario.addEventListener('submit', enviarEmail);
 
-    btnReset.addEventListener('click', function(e){
+    btnReset.addEventListener('click', function (e) {
         e.preventDefault();
         resetFormulario();
     })
@@ -46,23 +49,24 @@ document.addEventListener('DOMContentLoaded', function () {
             spinner.classList.remove('flex');
             spinner.classList.add('hidden');
 
-            resetFormulario();
+            resetFormulario();// reincia el formulario gracias a la function de abajo resetFromulario
 
-            // Crear una alerta
+            // Crear una alerta para decir que paso
             const alertaExito = document.createElement('P');
             alertaExito.classList.add('bg-green-500', 'text-white', 'p-2', 'text-center', 'rounded-lg', 'mt-10', 'font-bold', 'text-sm', 'uppercase');
             alertaExito.textContent = 'Mensaje enviado correctamente';
 
-            formulario.appendChild(alertaExito);
+            formulario.appendChild(alertaExito);// agregamos la alerta de exito
 
             setTimeout(() => {
-                alertaExito.remove(); 
-            }, 3000);
-        }, 3000);
+                alertaExito.remove();
+            }, 3000);// elimina el alertExsito despues de 3 segundos
+        }, 2000);// spinner dura 3 segundos
     }
 
     // esta funcion nos trae el valor del elemento que escribimos en el input
     function validar(e) {
+
         if (e.target.value.trim() === '') { // .trim() elimina los espacios
             mostrarAlerta(`El campo ${e.target.id} es obligatorio`, e.target.parentElement) //${e.target.id} hace que alert diga el id del input que salimos
             email[e.target.name] = '';// reiniciamos el objeto
@@ -71,6 +75,15 @@ document.addEventListener('DOMContentLoaded', function () {
         }
 
         if (e.target.id === 'email' && !validarEmail(e.target.value)) {
+            mostrarAlerta('email no es valido', e.target.parentElement) // pasamos referencia despues del mesaje para que valide
+            email[e.target.name] = '';// reniciamos el objecto
+            comprobarEmail();
+            return;
+        }
+
+        //----------PARTE DE RETO IMPLEMENTAR CC---------------
+        // comprobamos con if si emailCC es diferente a validarEmailCC 
+        if (e.target.id === 'emailCC' && !validarEmailCC(e.target.value)) {
             mostrarAlerta('email no es valido', e.target.parentElement) // pasamos referencia despues del mesaje para que valide
             email[e.target.name] = '';// reniciamos el objecto
             comprobarEmail();
@@ -87,16 +100,18 @@ document.addEventListener('DOMContentLoaded', function () {
 
         comprobarEmail();
 
+
     }
+
 
     function mostrarAlerta(mensaje, referencia) {
         limpiarAlerta(referencia);
-        
+
         // Generar alerta en HTML
         const error = document.createElement('P');
         error.textContent = mensaje;
         error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center');
-       
+
         // Inyectar el error al formulario
         referencia.appendChild(error);
     }
@@ -104,13 +119,20 @@ document.addEventListener('DOMContentLoaded', function () {
     function limpiarAlerta(referencia) {
         // Comprueba si ya existe una alerta
         const alerta = referencia.querySelector('.bg-red-600');
-        if(alerta) {
+        if (alerta) {
             alerta.remove();
         }
     }
 
     function validarEmail(email) {
-        const regex =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+        const regex = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+        const resultado = regex.test(email);
+        return resultado;
+    }
+    //------PARTE RETO IMPLEMENTAR EMAILCC----------
+    // creamo funcion para validar emailcc
+    function validarEmailCC(email) {
+        const regex = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
         const resultado = regex.test(email);
         return resultado;
     }
@@ -119,7 +141,7 @@ document.addEventListener('DOMContentLoaded', function () {
         // console.log(email); vemos el array
         if (Object.values(email).includes('')) { // lado derecho del array lo que el usuario ha escrito, includes nos devuelve true minetras un parametro del array este vacio, cuando estan llenos devuelve false
             btnSubmit.classList.add('opacity-50');
-            btnSubmit.desabled = true;
+            btnSubmit.disabled = true;
             return
         }
         btnSubmit.classList.remove('opacity-50');
@@ -129,6 +151,7 @@ document.addEventListener('DOMContentLoaded', function () {
     function resetFormulario() {
         // reiniciar el objeto
         email.email = '';
+        email.emailCC = '';
         email.asunto = '';
         email.mensaje = '';
 
