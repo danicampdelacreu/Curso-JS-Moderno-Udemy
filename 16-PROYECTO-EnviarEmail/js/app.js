82f7d4fefc60c9256ff7409ce3ca24fc15e38f66
dani campdelacreu <122914770+danicampdelacreu@users.noreply.github.com>
2023-09-26 09:33:08 +0200

    Seleccionar elementos de interfaz

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -1,1 +1,11 @@
 //----PROYECTO ENVIO EMAIL----
+
+document.addEventListener('DOMContentLoaded', function () {
+    
+    // Seleccionar elementos interfaz
+    const inputEmail = document.querySelector('#email');
+    const inputAsunto = document.querySelector('#asunto');
+    const inputMensaje = document.querySelector('#mensaje');
+
+    //console.log(inputMensaje); comprobar que nos envie a log la info de cada const
+});
