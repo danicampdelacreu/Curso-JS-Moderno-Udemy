04457ca36b52e807534bdb5f608ccaf9ab19433b
dani campdelacreu <122914770+danicampdelacreu@users.noreply.github.com>
2023-09-26 18:29:17 +0200

    habilitar boton enviar

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -2,39 +2,58 @@
 
 document.addEventListener('DOMContentLoaded', function () {
 
+    const email = {
+        email: '',
+        asunto: '',
+        mensaje: ''
+    }
+
+
     // Seleccionar elementos interfaz
     const inputEmail = document.querySelector('#email');
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
     const formulario = document.querySelector('#formulario');
+    const btnSubmit = document.querySelector('#formulario button[type="submit"]');
 
     //console.log(inputMensaje); comprobar que nos envie a log la info de cada input
 
     // Assingar eventos 
 
-    // (blur) cada vez que salimos del input
+    // (blur) cada vez que salimos del campo (mas lento)
     // (input) cada vez que escribamos en el input (validacion a tiempo real)
 
-    inputEmail.addEventListener('blur', validar);// llamamos a la funcion cuando suceda el evento si ponemos validar() la llamamos sin que suceda el evento
+    inputEmail.addEventListener('input', validar);// llamamos a la funcion cuando suceda el evento si ponemos validar() la llamamos sin que suceda el evento
 
-    inputAsunto.addEventListener('blur', validar);
+    inputAsunto.addEventListener('input', validar);
 
-    inputMensaje.addEventListener('blur', validar);
+    inputMensaje.addEventListener('input', validar);
 
     // esta funcion nos trae el valor del elemento que escribimos en el input
     function validar(e) {
         if (e.target.value.trim() === '') { // .trim() elimina los espacios
             mostrarAlerta(`El campo ${e.target.id} es obligatorio`, e.target.parentElement) //${e.target.id} hace que alert diga el id del input que salimos
+            email[e.target.name] = "";// reiniciamos el objeto
+            comprobarEmail();
             return;// detiene la ejecucion del codigo cuando if es true
-        } 
+        }
 
-        if(e.target.id === 'email' && !validarEmail(e.target.value)){
-            mostrarAlerta('email no es valido',e.target.parentElement) // pasamos referencia despues del mesaje para que valide
+        if (e.target.id === 'email' && !validarEmail(e.target.value)) {
+            mostrarAlerta('email no es valido', e.target.parentElement) // pasamos referencia despues del mesaje para que valide
+            email[e.target.name] = "";// reniciamos el objecto
+            comprobarEmail();
             return;
         }
 
         limpiarAlerta(e.target.parentElement);
-       
+
+        // asignar valores 
+
+        email[e.target.name] = e.target.value.trim().toLowerCase();
+
+        // comprobar el objeto email
+
+        comprobarEmail();
 
     }
 
@@ -56,7 +75,7 @@ document.addEventListener('DOMContentLoaded', function () {
         referencia.appendChild(error) // le decimos a formulario que agregue un hijo que sera (error) 
     }
 
-    function limpiarAlerta(referencia){
+    function limpiarAlerta(referencia) {
         const alerta = referencia.querySelector('.bg-red-600');
         if (alerta) { // alerta con referencia nos revisar si hay classe .bg-red-600 en el div que estamos saliendo
             alerta.remove();// con el remove hace que nos elimine la alerta previa del div que hemos salido cuando se nos generan multiples alertes
@@ -64,8 +83,19 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     function validarEmail(email) {
-        const regex =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+        const regex = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
         const resultado = regex.test(email)
         return resultado;
+    }
+
+    function comprobarEmail() {
+        // console.log(email); vemos el array
+        if (Object.values(email).includes('')) { // lado derecho del array lo que el usuario ha escrito, includes nos devuelve true minetras un parametro del array este vacio, cuando estan llenos devuelve false
+            btnSubmit.classList.add('opacity-50');
+            btnSubmit.desabled = true;
+            return;
+        }
+        btnSubmit.classList.remove('opacity-50');
+        btnSubmit.desabled = false;
     }
 });
