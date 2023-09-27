3478a44e0ea59e072d8d670bf406c1e95420af4c
dani campdelacreu <122914770+danicampdelacreu@users.noreply.github.com>
2023-09-27 11:05:32 +0200

    listeners de busqueda

--- 17-PROYECTO-Buscador/js/app.js
+++ 17-PROYECTO-Buscador/js/app.js
@@ -1,11 +1,31 @@
 //----------PROYECTO BUSCADOR COCHES------------//
 
 // Variable
+const marca = document.querySelector('#marca');
+const year= document.querySelector('#year');
+const minimo = document.querySelector('#minimo');
+const maximo = document.querySelector('#maximo');
+const puertas = document.querySelector('#puertas');
+const transmision = document.querySelector('#transmision');
+const color= document.querySelector('#color');
+
+// contenedor para resultado
 const resultado = document.querySelector('#resultado');
-const year = document.querySelector('#year');
+
 const max = new Date().getFullYear(); // maximo año actual
 const min = max - 15; // minimo año 10 menos del acutal
 
+// generar objeto con la busqueda
+
+const datosBusqueda = {
+    marca: '',
+    year: '',
+    minimo: '',
+    maximo: '',
+    puertas: '',
+    transmision: '',
+    color: '',
+}
 
 // eventos
 document.addEventListener('DOMContentLoaded', ()=>{
@@ -13,6 +33,38 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
     //Llena las opaciones de años
     llenarSelect();
+})
+
+// Event listener para los select de busqueda
+
+marca.addEventListener('change', e => {
+    datosBusqueda.marca = e.target.value;
+})
+
+year.addEventListener('change', e => {
+    datosBusqueda.year = e.target.value;
+
+})
+
+minimo.addEventListener('change', e => {
+    datosBusqueda.minimo = e.target.value;
+})
+
+maximo.addEventListener('change', e => {
+    datosBusqueda.maximo = e.target.value;
+})
+
+puertas.addEventListener('change', e => {
+    datosBusqueda.puertas = e.target.value;
+})
+
+transmision.addEventListener('change', e => {
+    datosBusqueda.transmision = e.target.value;
+})
+
+color.addEventListener('change', e => {
+    datosBusqueda.color = e.target.value;
+    //console.log(datosBusqueda); revisar que todo funcione
 })
 
 
