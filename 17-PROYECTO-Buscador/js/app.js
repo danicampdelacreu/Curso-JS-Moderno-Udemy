e67e623de1f3a2cef62449eb8d35a5c5e63958d4
dani campdelacreu <122914770+danicampdelacreu@users.noreply.github.com>
2023-09-27 10:31:17 +0200

    traer autos de otro js y mostrar

--- 17-PROYECTO-Buscador/js/app.js
+++ 17-PROYECTO-Buscador/js/app.js
@@ -1,1 +1,27 @@
 //----------PROYECTO BUSCADOR COCHES------------//
+
+// Variable
+const resultado = document.querySelector('#resultado');
+
+// eventos
+document.addEventListener('DOMContentLoaded', ()=>{
+    mostrarAutos();
+})
+
+
+// funciones
+function mostrarAutos(){
+    autos.forEach(auto =>{
+        const { marca, modelo, year, precio, puertas, color, transmision} = auto;
+        const autoHTML = document.createElement('P');
+
+        autoHTML.textContent = `
+            ${marca} ${modelo} - ${year} - ${puertas} PUERTAS  - transmision : ${transmision} -
+            Precio: ${precio} - Color: ${color}
+        `;
+
+
+        // insertar resultado
+        resultado.appendChild(autoHTML)
+    })
+}
