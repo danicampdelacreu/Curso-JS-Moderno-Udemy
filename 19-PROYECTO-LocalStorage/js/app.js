4c2436d1720ebf442fe82f66194b3346e66091a2
dani campdelacreu <122914770+danicampdelacreu@users.noreply.github.com>
2023-10-02 10:18:13 +0200

    Mensaje error, ver pantalla tweets

--- 19-PROYECTO-LocalStorage/js/app.js
+++ 19-PROYECTO-LocalStorage/js/app.js
@@ -19,5 +19,70 @@ function EventListenners() {
 function agregarTweet(e) {
     e.preventDefault();
 
-    console.log('agregando tweet');
+    //Textarea donde el usuario escribe
+    const tweet = document.querySelector('#tweet').value;
+    
+    // validacion...
+    if(tweet === ''){
+      mostrarError('Un tweet no puede enviarse vacio');
+        return;// evita ejecuten mas lineas de codigo
+    }
+
+    const tweetObj = {
+        id:  Date.now(),
+        tweet // = tweet : tweet
+    }
+    // añdir al arreglo de tweets
+    tweets = [...tweets, tweetObj];
+    // una vez agregado cremos HTML
+    crearHTML();
+
+    // reiniciar el fromulario
+
+    formulario.reset();
+
 }
+
+ // mostrar mensaje de error
+ function mostrarError(error) {
+    const mensajeError = document.createElement('p');
+    mensajeError.textContent = error;
+    mensajeError.classList.add('error');
+
+    //insertar en el contendio
+    const contenido = document.querySelector('#contenido');
+    contenido.appendChild(mensajeError);
+
+    // elimina alert despues de 3 segundos
+    setTimeout(() => {
+        mensajeError.remove();
+    }, 3000);
+ }
+
+ // Muestra listado tweets
+
+ function crearHTML() {
+
+    limpiarHTML();
+
+    if(tweets.length > 0){
+        tweets.forEach(tweet =>{
+            //crear HTML
+            const li = document.createElement('li');
+
+            // añadir el texto
+            li.innerText = tweet.tweet;
+            
+            // insertar en el html
+            listaTweets.appendChild(li);
+        });
+    }
+ }
+
+ // limpar HTML
+
+ function limpiarHTML() {
+    while( listaTweets.firstChild){
+        listaTweets.removeChild(listaTweets.firstChild);
+    }
+ }
