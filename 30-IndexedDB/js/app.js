add372331f3e4a6dec62ff0ef44a8c02429f46fa
dani campdelacreu <122914770+danicampdelacreu@users.noreply.github.com>
2023-11-10 12:34:01 +0100

    explicacion indexdb

--- 30-IndexedDB/js/app.js
+++ 30-IndexedDB/js/app.js
@@ -1,1 +1,10 @@
 // IndexedDB //
+
+/* 
+- Api en JS para almacenar grandes datos
+- Almacenar String, booleanos, inlcuso archivos
+- No tiene limites, + de 50mb pide permiso pero puede
+- Soporta las ultimas versiones de navegadores
+- BBDD base datos completa, van a estar visbles en una pestaña en el navegador, Cuidado con passwords...
+- 
+*/
