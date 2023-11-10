d17da6862dec1ea4639327f0d72b48437ce8253f
dani campdelacreu <122914770+danicampdelacreu@users.noreply.github.com>
2023-11-10 12:49:23 +0100

    indexedDB explicacion inicial

--- 30-IndexedDB/js/app.js
+++ 30-IndexedDB/js/app.js
@@ -8,3 +8,28 @@
 - BBDD base datos completa, van a estar visbles en una pestaña en el navegador, Cuidado con passwords...
 - 
 */
+
+document.addEventListener('DOMContentLoaded', () => {
+    crmDB();
+})
+
+function crmDB() {
+    // crear bbdd version 1.0
+    let crmDB = window.indexedDB.open('crm', 1);
+
+    // si hay error
+    crmDB.onerror = function() {
+        console.log('Hubo un error al crear BBDD');
+    }
+
+    // si se creo bien
+    crmDB.onsuccess = function () {
+        console.log('Base de datos Creada correctamente');
+    }
+    // configuracion de la BBDD
+    crmDB.onupgradeneeded = function (){
+        console.log('Este metodo solo se ejecuta una vez');
+    }
+}
+
+// Abrimos consola, escribimos window.indexDB y nos da todos los metodos que hay en indesDB
