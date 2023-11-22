3fe940b762eab3805179321b179b028a9391e51b
dani campdelacreu <122914770+danicampdelacreu@users.noreply.github.com>
2023-11-22 10:36:04 +0100

    editar e eliminar clientes

--- 32-PROYECTO-CRMIndexedDB/js/app.js
+++ 32-PROYECTO-CRMIndexedDB/js/app.js
@@ -1,54 +1,84 @@
 // APP PROYECTO INDEXEDB
 
-(function() {
+(function () {
     let DB;
+    const listadoClientes = document.querySelector('#listado-clientes');
 
     document.addEventListener('DOMContentLoaded', () => {
         crearDB();
-    
-        if(window.indexedDB.open('crm', 1)) {
+
+        if (window.indexedDB.open('crm', 1)) {
             obtenerClientes();
         }
-        
+
+        listadoClientes.addEventListener('click', eliminarRegistro);
     });
-    
+
+    // eliminar cliente creado
+    function eliminarRegistro(e) {
+        if (e.target.classList.contains('eliminar')) {
+            const idEliminar = Number(e.target.dataset.cliente);
+
+            const confirmar = confirm('Deseas eliminar el cliente?')
+
+            if (confirmar) {
+                const transaction = DB.transaction(['crm'], readwrite);
+                const objectStore = transaction.objectStore('crm');
+
+                objectStore.delete(idEliminar);
+
+                transaction.oncomplete = function () {
+                    console.log('Eliminado..');
+
+                    e.target.parentElement.parentElement.remove();
+                };
+
+                transaction.onerror = function () {
+                    console.log('Hubo un error');
+                }
+
+            }
+        }
+    }
+
+
     // Código de IndexedDB
     function crearDB() {
         // crear base de datos con la versión 1
         const crearDB = window.indexedDB.open('crm', 1);
-    
+
         // si hay un error, lanzarlo
-        crearDB.onerror = function() {
+        crearDB.onerror = function () {
             console.log('Hubo un error');
         };
-    
+
         // si todo esta bien, asignar a database el resultado
-        crearDB.onsuccess = function() {
+        crearDB.onsuccess = function () {
             // guardamos el resultado
             DB = crearDB.result;
         };
-    
+
         // este método solo corre una vez
-        crearDB.onupgradeneeded = function(e) {
+        crearDB.onupgradeneeded = function (e) {
             // el evento que se va a correr tomamos la base de datos
             const db = e.target.result;
-    
-            
+
+
             // definir el objectstore, primer parametro el nombre de la BD, segundo las opciones
             // keypath es de donde se van a obtener los indices
-            const objectStore = db.createObjectStore('crm', { keyPath: 'id',  autoIncrement: true } );
-    
+            const objectStore = db.createObjectStore('crm', { keyPath: 'id', autoIncrement: true });
+
             //createindex, nombre y keypath, 3ro los parametros
-            objectStore.createIndex('nombre', 'nombre', { unique: false } );
-            objectStore.createIndex('email', 'email', { unique: true } );
-            objectStore.createIndex('telefono', 'telefono', { unique: false } );
-            objectStore.createIndex('empresa', 'empresa', { unique: false } );
-            objectStore.createIndex('id', 'id', { unique: true } );
-    
-            
+            objectStore.createIndex('nombre', 'nombre', { unique: false });
+            objectStore.createIndex('email', 'email', { unique: true });
+            objectStore.createIndex('telefono', 'telefono', { unique: false });
+            objectStore.createIndex('empresa', 'empresa', { unique: false });
+            objectStore.createIndex('id', 'id', { unique: true });
+
+
             console.log('Database creada y lista');
         };
-    
+
     }
 
 
@@ -57,12 +87,12 @@
         const abrirConexion = window.indexedDB.open('crm', 1);
 
         // si hay un error, lanzarlo
-        abrirConexion.onerror = function() {
+        abrirConexion.onerror = function () {
             console.log('Hubo un error');
         };
-    
+
         // si todo esta bien, asignar a database el resultado
-        abrirConexion.onsuccess = function() {
+        abrirConexion.onsuccess = function () {
             // guardamos el resultado
             DB = abrirConexion.result;
 
@@ -70,16 +100,16 @@
 
 
             // retorna un objeto request o petición, 
-            objectStore.openCursor().onsuccess = function(e) {
-                 // cursor se va a ubicar en el registro indicado para accede ra los datos
-                 const cursor = e.target.result;
+            objectStore.openCursor().onsuccess = function (e) {
+                // cursor se va a ubicar en el registro indicado para accede ra los datos
+                const cursor = e.target.result;
 
                 //  console.log(e.target);
-     
-                 if(cursor) {
+
+                if (cursor) {
                     const { nombre, empresa, email, telefono, id } = cursor.value;
-                    
-                    const listadoClientes = document.querySelector('#listado-clientes');
+
+
                     listadoClientes.innerHTML += `
 
                         <tr>
@@ -95,16 +125,16 @@
                             </td>
                             <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5">
                                 <a href="editar-cliente.html?id=${id}" class="text-teal-600 hover:text-teal-900 mr-5">Editar</a>
-                                <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900">Eliminar</a>
+                                <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900 eliminar">Eliminar</a>
                             </td>
                         </tr>
                     `;
-        
+
                     cursor.continue();// traer el siguiento objeto uno tras otro
-                 } else {
+                } else {
                     //  console.log('No hay mas registros...');
-                 }
-             };
+                }
+            };
 
 
 
@@ -112,6 +142,6 @@
 
 
     }
-    
+
 
 })();
