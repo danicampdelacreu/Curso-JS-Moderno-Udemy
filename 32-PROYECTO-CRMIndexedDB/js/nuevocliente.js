8a84fc885fe643bd8bc45d7703508c869bfece05
dani campdelacreu <122914770+danicampdelacreu@users.noreply.github.com>
2023-11-22 09:30:40 +0100

    Aviso error

--- 32-PROYECTO-CRMIndexedDB/js/nuevocliente.js
+++ 32-PROYECTO-CRMIndexedDB/js/nuevocliente.js
@@ -39,8 +39,7 @@
         const empresa = document.querySelector('#empresa').value;
 
         if(nombre === '' || email === '' || telefono === '' || empresa === '') {
-             
-
+            imprimirAlerta('Todos los campos son obligatorios', 'error')  
             return;
         }
 
@@ -90,27 +89,32 @@
     }
 
     function imprimirAlerta(mensaje, tipo) {
-         // Crea el div
-
-         const divMensaje = document.createElement('div');
-         divMensaje.classList.add( "px-4", "py-3", "rounded",  "max-w-lg", "mx-auto", "mt-6", "text-center" );
-
-         if(tipo === 'error') {
-            divMensaje.classList.add('bg-red-100', "border-red-400", "text-red-700");
-         } else {
-             divMensaje.classList.add('bg-green-100', "border-green-400", "text-green-700");
+         const alerta = document.querySelector('.alerta');
+         if(!alerta){
+            //crear alerta
+            const divMensaje = document.createElement('div');
+            divMensaje.classList.add( "px-4", "py-3", "rounded",  "max-w-lg", "mx-auto", "mt-6", "text-center", "border", 'alerta' );
+   
+            if(tipo === 'error') {
+               divMensaje.classList.add('bg-red-100', "border-red-400", "text-red-700");
+            } else {
+                divMensaje.classList.add('bg-green-100', "border-green-400", "text-green-700");
+            }
+            
+            // Mensaje de error
+            divMensaje.textContent = mensaje;
+    
+            // Insertar en el DOM
+           formulario.appendChild(divMensaje);
+    
+            // Quitar el alert despues de 3 segundos
+            setTimeout( () => {
+                divMensaje.remove();
+            }, 3000);
          }
-         
-         // Mensaje de error
-         divMensaje.textContent = mensaje;
- 
-         // Insertar en el DOM
-        formulario.appendChild(divMensaje);
- 
-         // Quitar el alert despues de 3 segundos
-         setTimeout( () => {
-             divMensaje.remove();
-         }, 3000);
+
+        
+        
     }
 
 })();
