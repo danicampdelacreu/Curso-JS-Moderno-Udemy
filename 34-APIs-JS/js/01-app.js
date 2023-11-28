d48595a1151e2b58b22131e82c9c176b86a28e71
dani campdelacreu <122914770+danicampdelacreu@users.noreply.github.com>
2023-11-28 16:25:44 +0100

    notificacion envio a web

--- 34-APIs-JS/js/01-app.js
+++ 34-APIs-JS/js/01-app.js
@@ -5,15 +5,21 @@ const notificarBtn = document.querySelector('#notificar');
 notificarBtn.addEventListener('click', () => {
     Notification
         .requestPermission()
-        .then( res  => {
+        .then(res => {
             console.log('el resultado es', res);
         })
 })
 
 const verNotificacion = document.querySelector('#verNotificacion');
 verNotificacion.addEventListener('click', () => {
-    if(Notification.permission === 'granted'){
-        new Notification('Esta es la notificacion')
+    if (Notification.permission === 'granted') {
+        const notificacion = new Notification('Esta es la notificacion', {
+            icon: 'img/ccj.png',
+            body: 'Codigo de Dani'
+        });
         console.log('Se notifico correcto');
+        notificacion.onclick = function(){
+            window.open('http://www.codigoconjuan.com')
+        }
     }
 });
