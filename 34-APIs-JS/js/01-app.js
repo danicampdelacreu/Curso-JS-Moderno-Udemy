7597696409546ec8250d904dc68f2907eee65b1d
dani campdelacreu <122914770+danicampdelacreu@users.noreply.github.com>
2023-11-28 16:04:13 +0100

    enviar notificacion api

--- 34-APIs-JS/js/01-app.js
+++ 34-APIs-JS/js/01-app.js
@@ -1,1 +1,11 @@
 // APi's Notificacion //
+
+const notificarBtn = document.querySelector('#notificar');
+
+notificarBtn.addEventListener('click', () => {
+    Notification
+        .requestPermission()
+        .then( res  => {
+            console.log('el resultado es', res);
+        })
+})
