eff784518c14690f4b03cabc76e6f98022a34b9b
dani campdelacreu <122914770+danicampdelacreu@users.noreply.github.com>
2023-12-20 11:09:11 +0100

    Validacion i alerta buscador

--- 38-PROYECTO-PixabayImagenes/js/app.js
+++ 38-PROYECTO-PixabayImagenes/js/app.js
@@ -1,2 +1,43 @@
 // INICIO PROYECTO API IMAGENES //
 // KEY API = 41364789-abb2225bbfc3faae3dc582457 //
+
+const resultado  = document.querySelector('#resultado');
+const formulario  = document.querySelector('#formulario');
+
+window.onload = () => {
+    formulario.addEventListener('submit', validarFormulario);
+}
+
+function validarFormulario(e){
+    e.preventDefault();
+
+    const terminoBusqueda= document.querySelector('#termino').value;
+
+    if(terminoBusqueda === ''){
+        mostrarAlerta('Agregar un termino de busqueda');
+        return;
+    }
+}
+
+function mostrarAlerta(mensaje){
+
+    const exsisteAlerta = document.querySelector('.bg-red-100');
+
+    if(!exsisteAlerta) {
+        const alerta = document.createElement('p');
+    alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded', 'max-w-lg', 'mx-auto', 'mt-6', 'text-center');
+
+    alerta.innerHTML = `
+    <strong class= "font-bold">Error!</strong>
+    <span class="block sm:inline">${mensaje}</span>
+    `;
+
+    formulario.appendChild(alerta);
+
+    setTimeout(() => {
+        alerta.remove();
+    }, 3000);
+    }
+
+    
+}
