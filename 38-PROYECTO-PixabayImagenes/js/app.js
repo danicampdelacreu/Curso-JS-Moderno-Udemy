ea679eeb24c0c5d6fc1df1ff653f88b1192e2403
dani campdelacreu <122914770+danicampdelacreu@users.noreply.github.com>
2023-12-20 13:06:14 +0100

    cambiar a 30 imagenes por pagina

--- 38-PROYECTO-PixabayImagenes/js/app.js
+++ 38-PROYECTO-PixabayImagenes/js/app.js
@@ -5,7 +5,7 @@ const resultado  = document.querySelector('#resultado');
 const formulario  = document.querySelector('#formulario');
 const paginacionDiv  = document.querySelector('#paginacion');
 
-const registrosPorPagina = 40;
+const registrosPorPagina = 30;
 let totalPaginas;
 let iterador;
 let paginaActual = 1;
